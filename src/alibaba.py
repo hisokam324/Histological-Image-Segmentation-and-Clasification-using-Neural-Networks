cfa91c688d732bfedde394df3f411fbf75ea803b
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-08 13:04:35 -0300

    .

---
+++ src/alibaba.py
@@ -0,0 +1,17 @@
+"""
+Se implementan dos funciones basicas
+"""
+
+def get_empty():
+    """
+    Esta funcion no pide nada y devuelve una lista vacia
+    """
+    out = []
+    return out
+
+def quinty(input):
+    """
+    Esta funcion pide un input y lo multiplica por 5
+    """
+    out = input*5
+    return out
