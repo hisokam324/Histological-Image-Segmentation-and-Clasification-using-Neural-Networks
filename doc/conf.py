8f54762fafac5d249e3289b5666e2d69614976ea
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-10 19:56:37 -0300

    .

--- doc/conf.py
+++ doc/conf.py
@@ -20,6 +20,8 @@ release = '1.0.0'
 
 extensions = ['sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
+autodoc_mock_imports = ["torch", "matplotlib", "src"]
+
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
