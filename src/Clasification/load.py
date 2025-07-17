565097ab5de9b2cf5d9fa3007099f45a6c3857b0
Rinaldo Almada <maurialmadab@gmail.com>
2025-07-17 12:52:14 -0300

    .

--- src/Clasification/load.py
+++ src/Clasification/load.py
@@ -2,7 +2,26 @@ from medmnist import PathMNIST
 from torchvision import transforms
 from torch.utils.data import DataLoader
 
+"""
+Auxiliary module to load images
+"""
+
 def get_loaders(configuration, toLoad):
+    '''
+    This function load PathMNIST images directly from MedMNIST and create the corresponding loaders
+    
+    Args:
+        configuration (dict): Configuration information, such as batch size
+        
+        toLoad (boolean list): Indicates wich loaders to load, none load leaders will be return empty
+    
+    Returns:
+        Train loader (Loader): Loader meant for training
+        
+        Validation loader (Loader): Loader meant for validation
+        
+        Test loader (Loader): Loader meant for testing
+    '''
     batch_size = configuration["train"]["batch size"]
     download = True
 
